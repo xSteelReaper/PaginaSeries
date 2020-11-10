cd3949a43e28393de649af9470ce5d3ea12ec2e7
fcchidan <71665816+fcchidan@users.noreply.github.com>
2020-11-10 17:51:29 -0400

    agregado templates de registro y usuario

--- pagina/apps/Usuario/views.py
+++ pagina/apps/Usuario/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from .forms import RegistroForm
+
 
 # Create your views here.
+class RegistroUsuario(CreateView):
+    model = User
+    template_name = "Usuario/registrar.html"
+    form_class = RegistroForm
+    success_url = reverse_lazy('list_user')
+ 
+ 
+class UserList(ListView):
+    model = User
+    template_name = 'Usuario/list_user.html'
