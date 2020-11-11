4fcbed474c2fbd1a3f3ff90ea77a54941dba2163
Daniel Gonzalez <danielgonzalez3c@gmail.com>
2020-11-11 19:57:49 -0300

    reestructuracion de proyecto

--- pagina/apps/Usuario/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-from .views import RegistroUsuario, UserList
-
-urlpatterns = [
-    path('registrar', RegistroUsuario.as_view(), name="registrar"),
-    path('listar', UserList.as_view(), name="list_user"),
-]
--- pagina/pagina/urls.py
+++ pagina/pagina/urls.py
@@ -18,6 +18,4 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('registro/', include('apps.Registro.urls')),
-    path('usuario/', include('apps.Usuario.urls')),
 ]
