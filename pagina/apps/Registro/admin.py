298d55301dcb925117da804071b4ea23dde04651
Daniel Gonzalez <danielgonzalez3c@gmail.com>
2020-11-11 21:10:33 -0300

    Creacion modelos anime y manga

--- pagina/apps/Registro/admin.py
+++ pagina/apps/Registro/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Anime, Manga
 
-# Register your models here.
+
+admin.site.register(Anime)
+admin.site.register(Manga)
--- pagina/apps/Registro/models.py
+++ pagina/apps/Registro/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+TIPO = (
+    ('Shounen', 'Shounen'),
+    ('Shoujo', 'Shoujo'),
+    ('Seinen', 'Seinen'),
+    ('Josei', 'Josei'),
+)
+
+class Anime(models.Model):
+    nombre = models.CharField(max_length=100)
+    anioPublicacion = models.IntegerField()
+    tipo = models.CharField(max_length=50, choices=TIPO)
+    sinopsis = models.CharField(max_length=200)
+    fotografia = models.ImageField(upload_to='catalogo_anime', blank=True)
+    
+    def __str__(self):
+        return str(self.fotografia)
+
+ESTADO = (
+    ('Emision', 'Emision'),
+    ('Finalizado', 'Finalizado'),
+)
+
+class Manga(models.Model):
+    nombre = models.CharField(max_length=100)
+    anioPublicacion = models.IntegerField()
+    estado = models.CharField(max_length=50, choices=ESTADO)
+    sinopsis = models.CharField(max_length=200)
+    fotografia = models.ImageField(upload_to='catalogo_manga', blank=True)
+    
+    def __str__(self):
+        return str(self.fotografia)
