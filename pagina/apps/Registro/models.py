afb01b17c5ed31a2fb00f03a688481d24c5b262b
Daniel Gonzalez <danielgonzalez3c@gmail.com>
2020-11-12 22:39:26 -0300

    Se agregan carpetas con imagenes correspondientes

--- pagina/apps/Registro/models.py
+++ pagina/apps/Registro/models.py
@@ -11,7 +11,7 @@ class Anime(models.Model):
     nombre = models.CharField(max_length=100)
     anioPublicacion = models.IntegerField()
     tipo = models.CharField(max_length=50, choices=TIPO)
-    sinopsis = models.CharField(max_length=200)
+    sinopsis = models.CharField(max_length=1000)
     fotografia = models.ImageField(upload_to='catalogo_anime', blank=True)
     
     def __str__(self):
@@ -26,7 +26,7 @@ class Manga(models.Model):
     nombre = models.CharField(max_length=100)
     anioPublicacion = models.IntegerField()
     estado = models.CharField(max_length=50, choices=ESTADO)
-    sinopsis = models.CharField(max_length=200)
+    sinopsis = models.CharField(max_length=1000)
     fotografia = models.ImageField(upload_to='catalogo_manga', blank=True)
     
     def __str__(self):
